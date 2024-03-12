3209dd1ae9e278b9e9a20677262efb9b5d5c94d6
Hung Le Huy <63230297+trumpiter-max@users.noreply.github.com>
2024-03-12 16:35:32 +0700

    update backend docker & add mui to client

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,46 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './resources/images/logo.svg';
 import './App.css';
+
+// Material components
+import Container from '@mui/material/Container';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Link from '@mui/material/Link';
+import { makeStyles } from '@emotion/styled';
+
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
+import CssBaseline from '@mui/material/CssBaseline';
+
+function Copyright() {
+  return (
+    <Typography variant="body2" color="text.secondary" align="center">
+      {'Copyright of '}
+      <Link color="inherit" href="#">
+        Remise
+      </Link>{'© '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
+}
 
 function App() {
   return (
     <div className="App">
+      <CssBaseline />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Hello From Docker For React
         </p>
-      </header>
+      </header> 
+      <Copyright />
     </div>
   );
 }
---
+++ client/src/styles/styles.js
@@ -0,0 +1,15 @@
+import { makeStyles } from '@mui/styles';
+
+const styles = () => {
+    return {
+        //box: {
+        //   backgroundColor: "red"
+        // },
+        // text: {
+        //  color: "pink"
+        //}
+    };
+};
+
+const useStyles = makeStyles(styles);
+export default useStyles; 
