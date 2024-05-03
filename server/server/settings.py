6a56d7009d34593feb2c30546c250ab7b6e68365
Hung Le Huy <63230297+trumpiter-max@users.noreply.github.com>
2024-05-03 15:08:04 +0700

    fix static path, script and settings

--- server/server/settings.py
+++ server/server/settings.py
@@ -151,18 +151,19 @@ USE_I18N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/5.0/howto/static-files/
+STATIC_URL = '/static/'
 
-STATIC_URL = "static/"
+# The absolute path to the directory where collectstatic will collect static files for deployment.
+STATIC_ROOT = '/var/www/server/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-
+# Ensure that the STATICFILES_DIRS list does not contain the STATIC_ROOT
+STATICFILES_DIRS = [
+    # Your other directories containing static files for development
+    # Example: BASE_DIR / 'assets',
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-STATICFILES_DIRS = [ 
-    BASE_DIR/STATIC_URL, 
-]
+
--- server/server/urls.py
+++ server/server/urls.py
@@ -14,12 +14,19 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path,include
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 urlpatterns = [
-    path('api/v1/admin/', admin.site.urls),
-    path('api/v1/authentication/',include('authentication.urls'))
+    path('admin/', admin.site.urls),
+    path('api/v1/authentication/',include('authentication.urls')),
     path("api/v1/management/", include("management.urls")),
-]
+    path("api/v1/token/", TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path("api/v1/token/refresh/", TokenRefreshView.as_view(), name='token_refresh'),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
