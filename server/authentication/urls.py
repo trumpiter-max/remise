67e2ad9e27ed7ab013e87c9f40d364fe8e64da2e
still <qanh7661@gmail.com>
2024-05-21 22:45:50 +0700

    update reset-password

--- server/authentication/urls.py
+++ server/authentication/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
 from . import views
+from .views import  RequestPasswordResetEmail,PasswordResetConfirmView
 
 urlpatterns = [
 	path('register', views.UserRegister.as_view(), name='register'),
 	path('login', views.UserLogin.as_view(), name='login'),
 	path('logout', views.UserLogout.as_view(), name='logout'),
-	path('user', views.UserView.as_view(), name='user'),
+	path('user', views.UserView.as_view(), name='user'),   
+    path('request-reset-email/', RequestPasswordResetEmail.as_view(),
+         name="request-reset-email"),
+    path('password-reset-confirm/<uidb64>/<token>/', PasswordResetConfirmView.as_view(), name='password-reset-confirm'),
 ]
